9ede68eda5a82e6b7eebb966380d4303b2e29648
Connor Genyk <genyk-c@webmail.uwinnipeg.ca>
2017-12-20 00:43:42 -0600

    PSUH

--- src/User_Interface.java
+++ src/User_Interface.java
@@ -1,42 +1,93 @@
-
-
 // import java libraries here as needed
 
 import javax.swing.*;
 import java.io.*; 					// import file io libraries
 import java.util.*;
 import java.lang.String;
-import com.sun.org.apache.xalan.internal.lib.ExsltStrings.*;
 import java.lang.*;
 import java.text.*;
-public class User_Interface {  // begin class
+
+// JOptionPane.showInputDialog(null, "How many racers?")
+
+class SkatingRaces {  // begin class
+
+    static class Skater {
+        private int id;
+        private int[] times;
+        // constructor
+        public Skater(int id, int[] times) {
+            this.id = id;
+            this.times = times;
+        }
+
+        public String toString() {
+            String times_str = "";
+            DecimalFormat df1 = new DecimalFormat("00");
+            for(int i = 0; i < 20; i+= 2){
+                times_str += df1.format(Integer.toString(times[i])) + ":" + df1.format(Integer.toString(times[i+1])) + " ";
+            }
+            return Integer.toString(id) + " " + times_str;
+        }
+    }
+
+    private static List<Skater> skaters = new ArrayList<Skater>(); // Makes the array
+
+    public static void loadFile() throws FileNotFoundException {
+        String token1 = ""; // Reading the file
+        String delim = "[ :]+";        // delimiter string for splitting input string
+
+        Scanner fin = new Scanner(new FileReader("skaterData.txt"));
+
+        int id = 0;
+        // while loop
+        while (fin.hasNextLine()) {
+            // find next line
+            token1 = fin.nextLine();
+
+            String times_str[] = token1.split(delim);
+            int[] times = new int[20];
+            for(int i = 0; i < times_str.length; i++){
+                times[i] = Integer.parseInt(times_str[i]);
+            }
+            Skater skater = new Skater(id, times);
+
+            skaters.add(skater);
+            System.out.println(skater.toString());
+            id++;
+        }
+        fin.close(); // closes file in
+    }
+
+
 
     public static void main(String[] args) throws IOException, ParseException {  // begin main
+        loadFile();
 
         // ********* declaration of constants **********
 
         // ********** declaration of variables **********
 
-        String strin;                // string data input from keyboard
-        String strout;                // processed info string to be output
-        String bannerOut;            // string to print banner to message dialogs
+        //String strin;                // string data input from keyboard
+        //String strout;                // processed info string to be output
+        //String bannerOut;            // string to print banner to message dialogs
 
-        String prompt;                // prompt for use in input dialogs
+        //String prompt;                // prompt for use in input dialogs
 
-        String delim = "[ :]+";        // delimiter string for splitting input string
-        String tabSpace = "      ";    // six spaces
-        int counter = 1; // Counts the frequancy
-        int curnum = 0; // Current numbber
-        String token1 = ""; // Reading the file
-        int prevnum = 0; // Last number
+        //String delim = "[ :]+";        // delimiter string for splitting input string
+        //String tabSpace = "      ";    // six spaces
+        //int counter = 1; // Counts the frequancy
+        //int curnum = 0; // Current numbber
+        //String token1 = ""; // Reading the file
+        //int prevnum = 0; // Last number
         // create instances of objects for i/o and formatting
 
         //ConsoleReader console = new ConsoleReader(System.in);
         //DecimalFormat df1 = new DecimalFormat("$##.00");
 
-        //  BufferedReader fin = new BufferedReader(new FileReader("skaterData.txt"));
+        //BufferedReader fin = new BufferedReader(new FileReader("skaterData.txt"));
         // PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("testOut.txt")));
-        Scanner inf = new Scanner(new File("skaterData.txt"));
+        //Scanner inf = new Scanner(new File("skaterData.txt"));
+
         // ********** Print output Banner **********
 
 
@@ -58,61 +109,64 @@ public class User_Interface {  // begin class
         // String[] tokens = strin.split(delim);
 
 
+
+        //Scanner in = new Scanner(System.in);
         // ************************ processing ***************************
-        int n = 0;
-        n=   Integer.parseInt(JOptionPane.showInputDialog(null, "How many racers?"));
+        //int n = 0;
+        //n = Integer.parseInt(in.nextLine());
 
-        List<String> temps = new ArrayList<String>(); // Makes the array
-        String list = String.join(",", temps);
+        //List<String> temps = new ArrayList<String>(); // Makes the array
+        //String list = String.join(",", temps);
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print(list);
-        }
+        //for (int i = 0; i < 10; i++) {
+        //    System.out.print(list);
+        //}
 
 
         // while loop
-        while (inf.hasNextLine()) {
-            // find next line
-            token1 = inf.nextLine();
+        //while (inf.hasNextLine()) {
+        // find next line
+        //    token1 = inf.nextLine();
 
-            temps.add(token1);
+        //    temps.add(token1);
 
-        }
-        inf.close(); // closes file in
-
-
-        System.out.println(temps);
+        //}
+        //inf.close(); // closes file in
 
 
-        int id = 0;
-        int displayid = 1;
-
-        while (id < n) {
-            String addtime = JOptionPane.showInputDialog(null, "Enter Race Result for Racer: " + displayid, "Enter", JOptionPane.QUESTION_MESSAGE);
-
-            id++;
-            displayid++;
-            System.out.println("Id Number: " + id + " |" + " Times: " + list + addtime + " |" + " Avg" + "|" + " Speed" + "km/h");
-
-        }
+        // System.out.println(temps);
 
 
-       // DateFormat formatter = new SimpleDateFormat("HH:mm");
-      //  java.sql.Time timeValue = new java.sql.Time(formatter.parse(list).getTime());
-      //  System.out.println(timeValue);
+        //int id = 0;
+        //int displayid = 1;
 
-            // ************************ print output ****************************
+        //while (id < n) {
+        //    System.out.println("Enter Race Result for Racer: " + displayid);
+        //    String addtime = in.nextLine(); //JOptionPane.showInputDialog(null, "Enter Race Result for Racer: " + displayid, "Enter", JOptionPane.QUESTION_MESSAGE);
+
+        //    id++;
+        //    displayid++;
+        //    System.out.println("Id Number: " + id + " |" + " Times: " + list + addtime + " |" + " Avg" + "|" + " Speed" + "km/h");
+
+        //}
 
 
-            // ******** closing message *********
+        // DateFormat formatter = new SimpleDateFormat("HH:mm");
+        //  java.sql.Time timeValue = new java.sql.Time(formatter.parse(list).getTime());
+        //  System.out.println(timeValue);
 
-            System.out.println("end of processing.");
-            // fout.format("%n%nend of processing.");
+        // ************************ print output ****************************
 
-            // ***** close streams *****
 
-            //fin.close();			// close input buffer
-            //  fout.close();			// close output buffer
-        }  // end main
-    }  // end class
+        // ******** closing message *********
+
+        //System.out.println("end of processing.");
+        // fout.format("%n%nend of processing.");
+
+        // ***** close streams *****
+
+        //fin.close();			// close input buffer
+        //  fout.close();			// close output buffer
+    }  // end main
+}  // end class
 
