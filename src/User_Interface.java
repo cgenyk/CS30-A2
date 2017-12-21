2cd5999cd8864360573d5b0a4cd23fa626f5e178
Connor Genyk <genyk-c@webmail.uwinnipeg.ca>
2017-12-20 18:37:51 -0600

    !! HUGE COMMIT !! - Cleaned up code (somewhat) - Added new features - IT WORKS!

--- src/User_Interface.java
+++ src/User_Interface.java
@@ -88,12 +88,30 @@ class SkatingRaces {  // begin class
         public double getkm(int sec) {
 
             int timeinhr = sec * 60 * 60;
-            // System.out.println(get());
+
             //  for (int i = 0; i < times.length; i += 2) {
-            double km = 5 / timeinhr;
+            double km = 5.0 / timeinhr;
 
 
             return km;
+        }
+
+        public double getcuskm(int cussec) {
+
+
+            String time2 = JOptionPane.showInputDialog("Enter time in MM:SS format (CUS KM)");
+            String[] units = time2.split(":"); //will break the string up into an array
+            int cusminutes2 = Integer.parseInt(units[0]); //first element
+            int cusseconds2 = Integer.parseInt(units[1]); //second element
+            int duration = 60 * cusminutes2 + cusseconds2; //add up our values
+
+            int custimeinhr = duration * 60 * 60;
+
+            //  for (int i = 0; i < times.length; i += 2) {
+            double cuskm = 5.0 / custimeinhr;
+
+
+            return cuskm;
         }
 
 
@@ -124,17 +142,21 @@ class SkatingRaces {  // begin class
             fin.close();
         }
 
+
         public static void main(String[] args) throws IOException, ParseException {
             loadFile();
 
             DecimalFormat df1 = new DecimalFormat("00");
+            //DecimalFormat df2 = new DecimalFormat("00.##");
             System.out.println("Times: ");
 
+            Skater skater = null;
             for (int i = 0; i < skaters.size(); i++) {
-                Skater skater = skaters.get(i);
+                skater = skaters.get(i);
 
 
                 int total_seconds = skater.getCusNum();
+
                 int minutes = total_seconds / 60;
                 int seconds = total_seconds % 60;
 
@@ -150,125 +172,26 @@ class SkatingRaces {  // begin class
                 System.out.println(skater.id + ": " + df1.format(avminutes) + ":" + df1.format(avseconds));
 
 
-                System.out.println("Km: ");
-
+                System.out.println("Speed: ");
 
                 double total_km = skater.getkm(total_seconds);
+
 
                 System.out.println(skater.id + ": " + total_km);
 
                 System.out.println("---- NEW RACER ----");
+
             }
 
-
-            //  System.out.println(skater.id + ": " + df1.format(cminutes)+":"+df1.format(cseconds));
-
+            System.out.println("*** END OF RACER DATA | CUSTOM QUERY RESPONSES ***");
+            System.out.println("---- QUERY RESPONSE: CUSTOM SPEED ----");
+            int total_seconds = skater.getCusNum();
+            System.out.println(skater.getcuskm(total_seconds));
+            System.out.println("---- !! END OF !! QUERY RESPONSE: CUSTOM SPEED ----");
+            System.out.println("*** END OF CUSTOM QUERY RESPONSES *** | Program Stopped");
 
         }
 
-
-        // ********* declaration of constants **********
-
-        // ********** declaration of variables **********
-
-        //String strin;                // string data input from keyboard
-        //String strout;                // processed info string to be output
-        //String bannerOut;            // string to print banner to message dialogs
-
-        //String prompt;                // prompt for use in input dialogs
-
-        //String delim = "[ :]+";        // delimiter string for splitting input string
-        //String tabSpace = "      ";    // six spaces
-        //int counter = 1; // Counts the frequancy
-        //int curnum = 0; // Current numbber
-        //String token1 = ""; // Reading the file
-        //int prevnum = 0; // Last number
-        // create instances of objects for i/o and formatting
-
-        //ConsoleReader console = new ConsoleReader(System.in);
-        //DecimalFormat df1 = new DecimalFormat("$##.00");
-
-        //BufferedReader fin = new BufferedReader(new FileReader("skaterData.txt"));
-        // PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("testOut.txt")));
-        //Scanner inf = new Scanner(new File("skaterData.txt"));
-
-        // ********** Print output Banner **********
-
-
-        // ************************ get input **********************
-
-    /*	prompt = "Enter your prompt text here. \n";
-    	prompt += "you may need to add additional lines\n";
-    	prompt += "or delete some of these prompt lines.\n\n";
-
-
-    	strin = JOptionPane.showInputDialog(bannerOut + prompt);
-    */
-    	/* **********************************
-    	 * split the input string into tokens
-    	 * use the value in delim as the delimieter
-    	 * uncomment the line to use it.
-    	 ************************************/
-        // String[] tokens = strin.split(delim);
-
-
-        //Scanner in = new Scanner(System.in);
-        // ************************ processing ***************************
-        //int n = 0;
-        //n = Integer.parseInt(in.nextLine());
-
-        //List<String> temps = new ArrayList<String>(); // Makes the array
-        //String list = String.join(",", temps);
-
-        //for (int i = 0; i < 10; i++) {
-        //    System.out.print(list);
-        //}
-
-
-        // while loop
-        //while (inf.hasNextLine()) {
-        // find next line
-        //    token1 = inf.nextLine();
-
-        //    temps.add(token1);
-
-        //}
-        //inf.close(); // closes file in
-
-
-        // System.out.println(temps);
-
-
-        //int id = 0;
-        //int displayid = 1;
-
-        //while (id < n) {
-        //    System.out.println("Enter Race Result for Racer: " + displayid);
-        //    String addtime = in.nextLine(); //JOptionPane.showInputDialog(null, "Enter Race Result for Racer: " + displayid, "Enter", JOptionPane.QUESTION_MESSAGE);
-
-        //    id++;
-        //    displayid++;
-        //    System.out.println("Id Number: " + id + " |" + " Times: " + list + addtime + " |" + " Avg" + "|" + " Speed" + "km/h");
-
-        //}
-
-
-        // DateFormat formatter = new SimpleDateFormat("HH:mm");
-        //  java.sql.Time timeValue = new java.sql.Time(formatter.parse(list).getTime());
-        //  System.out.println(timeValue);
-
-        // ************************ print output ****************************
-
-
-        // ******** closing message *********
-
-        //System.out.println("end of processing.");
-        // fout.format("%n%nend of processing.");
-
-        // ***** close streams *****
-
-        //fin.close();			// close input buffer
-        //  fout.close();			// close output buffer
     }
 // end main
 }
